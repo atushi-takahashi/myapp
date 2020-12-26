0a7c38e39bfad752d486c5f91d878c677981d562
atushi-takahashi <ats19960723@icloud.com>
2020-12-27 01:07:08 +0900

    add[scaffold task (finish p.40)]

--- app/controllers/welcome_controller.rb
+++
@@ -1,4 +0,0 @@
-class WelcomeController < ApplicationController
-  def top
-  end
-end
--- test/controllers/welcome_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get top" do
-    get welcome_top_url
-    assert_response :success
-  end
-
-end
