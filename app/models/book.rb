dde77cd3b19254e1dd30805d473a2e10ad81f370
atushi-takahashi <ats19960723@icloud.com>
2020-12-30 10:24:58 +0900

    update[app-model=>book,author]

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,9 @@ class Book < ApplicationRecord
   scope :costly, -> { where("price > ?", 3000) }
 　# 名前で検索するメソッド
   scope :written_about, ->(theme) { where("name like ?", "%#{theme}%")}
-　# リレーションの定義　１対多
+　# リレーションの定義　多対１
   belongs_to :publisher
+  # リレーションの定義　多対多
+  has_many :book_authors
+  has_many :authors, through: :book_authors
 end
